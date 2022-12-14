1261206954de9d7eda49e5c37b681604989c5014
Andrey Tamplon <aatamplon@sbercloud.ru>
2022-12-15 01:29:33 +0700

    Fixed unlocking of other threads mutexes

--- tamplon/lab11/Makefile
+++
@@ -1,2 +0,0 @@
-run:
-	gcc -Wall -Wextra -Werror -pthread main.c -o synchronized_output.out
--- tamplon/lab11/main.c
+++ tamplon/lab11/main.c
@@ -2,103 +2,131 @@
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-pthread_mutex_t mutex1, mutex2, startMutex;
-void* threadFunction(void* args) {
-	(void)args;
-	if (pthread_mutex_lock(&startMutex) != 0) {
-		perror("pthread_mutex_lock: startMutex");
+#define NUMBER_OF_MUTEXES 3
+#define NUMBER_OF_ITERATIONS 10
+pthread_mutex_t mutexes[NUMBER_OF_MUTEXES];
+
+typedef struct {
+	char* thread_name;
+} thread_args;
+
+bool second_thread_is_ready = false;
+
+void destroy_mutex(int mutex_id) {
+	int error = pthread_mutex_destroy(&mutexes[mutex_id]);
+	if (error != 0) {
+		fprintf(stderr, "Error while destroying mutex %d: %s\n", mutex_id, strerror(error));
 		exit(EXIT_FAILURE);
 	}
-	for (int i = 0; i < 10; ++i) {
-		if (pthread_mutex_lock(&mutex2) != 0) {
-			perror("pthread_mutex_lock: mutex2");
-			exit(EXIT_FAILURE);
-		}
-		printf("child: %d\n", i);
-		if (pthread_mutex_unlock(&mutex1) != 0) {
-			perror("pthread_mutex_unlock: mutex1");
-			exit(EXIT_FAILURE);
-		}
-	}
-	if (pthread_mutex_unlock(&mutex2) != 0) {
-		perror("pthread_mutex_unlock: mutex2");
-		exit(EXIT_FAILURE);
-	}
-	if (pthread_mutex_unlock(&startMutex) != 0) {
-		perror("pthread_mutex_unlock: startMutex");
-		exit(EXIT_FAILURE);
-	}
-	pthread_exit(NULL);
 }
 
-void destroyMutexes() {
-	if (pthread_mutex_destroy(&mutex1) != 0) {
-		perror("Error destroying mutex1\n");
+void destroy_mutexes() {
+	for (int i = 0; i < NUMBER_OF_MUTEXES; ++i) {
+		destroy_mutex(i);
 	}
-	if (pthread_mutex_destroy(&mutex2) != 0) {
-		perror("Error destroying mutex2\n");
+}
+
+void lock_mutex(int mutex_id) {
+	int error = pthread_mutex_lock(&mutexes[mutex_id]);
+	if (error != 0) {
+		fprintf(stderr, "Error while locking mutex %d: %s\n", mutex_id, strerror(error));
+		destroy_mutexes();
+		exit(EXIT_FAILURE);
 	}
-	if (pthread_mutex_destroy(&startMutex) != 0) {
-		perror("Error destroying startMutex\n");
+}
+
+void unlock_mutex(int mutex_id) {
+	int error = pthread_mutex_unlock(&mutexes[mutex_id]);
+	if (error != 0) {
+		fprintf(stderr, "Error while unlocking mutex %d: %s\n", mutex_id, strerror(error));
+		destroy_mutexes();
+		exit(EXIT_FAILURE);
 	}
+}
+
+void init_mutexes() {
+	pthread_mutexattr_t attr;
+	if (pthread_mutexattr_init(&attr) != 0) {
+		perror("pthread_mutexattr_init");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK) != 0) {
+		perror("pthread_mutexattr_settype");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < NUMBER_OF_MUTEXES; ++i) {
+		if (pthread_mutex_init(&mutexes[i], &attr) != 0) {
+			perror("pthread_mutex_init");
+			for (int j = 0; j < i; ++j) {
+				destroy_mutex(j);
+			}
+			exit(EXIT_FAILURE);
+		}
+	}
+	/*
+	 * After a mutex attributes object has been used to initialize one or more mutexes,
+	 * any function affecting the attributes object (including destruction)
+	 * shall not affect any previously initialized mutexes
+	 */
+	if (pthread_mutexattr_destroy(&attr) != 0) {
+		perror("pthread_mutexattr_destroy");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void wait_for_other_thread() {
+	while (!second_thread_is_ready) {
+		usleep(1000);
+	}
+}
+
+void* printing_function(void* args) {
+	char* thread_name = ((thread_args*)args)->thread_name;
+	int current_mutex = 0, next_mutex = 1;
+
+	if (!second_thread_is_ready) {
+		second_thread_is_ready = true;
+		current_mutex = 2;
+		next_mutex = 0;
+		lock_mutex(current_mutex);
+	}
+
+	for (int i = 0; i < NUMBER_OF_ITERATIONS; ++i) {
+		lock_mutex(next_mutex);
+		printf("%d %s\n", i, thread_name);
+		unlock_mutex(current_mutex);
+
+		current_mutex = next_mutex;
+		next_mutex = (current_mutex + 1) % NUMBER_OF_MUTEXES;
+	}
+
+	unlock_mutex(current_mutex);
+	pthread_exit(NULL);
 }
 
 int main() {
+	init_mutexes();
+	lock_mutex(0);
 	pthread_t thread;
-	if (pthread_mutex_init(&startMutex, NULL) != 0) {
-		perror("Error creating mutex");
-		return EXIT_FAILURE;
+	thread_args child_args;
+	child_args.thread_name = "Child";
+
+	if (pthread_create(&thread, NULL, printing_function, &child_args) != 0) {
+		perror("pthread_create");
+		exit(EXIT_FAILURE);
 	}
-	if (pthread_mutex_init(&mutex1, NULL) != 0) {
-		perror("Error creating mutex");
-		if (pthread_mutex_destroy(&startMutex) != 0) {
-			perror("Error destroying startMutex\n");
-		}
-		return EXIT_FAILURE;
+	wait_for_other_thread();
+	thread_args parent_args;
+	parent_args.thread_name = "Parent";
+	printing_function(&parent_args);
+	if(pthread_join(thread, NULL) != 0) {
+		perror("pthread_join");
+		exit(EXIT_FAILURE);
 	}
-	if (pthread_mutex_init(&mutex2, NULL) != 0) {
-		perror("Error creating mutex");
-		if (pthread_mutex_destroy(&mutex1) != 0) {
-			perror("Error destroying mutex1\n");
-		}
-		if (pthread_mutex_destroy(&startMutex) != 0) {
-			perror("Error destroying startMutex\n");
-		}
-		return EXIT_FAILURE;
-	}
-	pthread_mutex_lock(&startMutex);
-	int err = pthread_create(&thread, NULL, threadFunction, NULL);
-	if (err != 0) {
-		perror("Error creating thread");
-		return EXIT_FAILURE;
-	}
-	if (pthread_mutex_lock(&mutex2) != 0) {
-		perror("pthread_mutex_lock: mutex2");
-		destroyMutexes();
-		return EXIT_FAILURE;
-	}
-	if (pthread_mutex_unlock(&startMutex) != 0) {
-		perror("pthread_mutex_unlock: startMutex");
-		destroyMutexes();
-		return EXIT_FAILURE;
-	}
-	for (int i = 0; i < 10; ++i) {
-		if (pthread_mutex_lock(&mutex1) != 0) {
-			perror("pthread_mutex_lock: mutex1");
-			return EXIT_FAILURE;
-		}
-		printf("parent: %d\n", i);
-		if (pthread_mutex_unlock(&mutex2) != 0) {
-			perror("pthread_mutex_unlock: mutex2");
-			return EXIT_FAILURE;
-		}
-	}
-	if (pthread_join(thread, NULL) != 0) {
-		destroyMutexes();
-		perror("Error joining thread");
-		return EXIT_FAILURE;
-	}
-	destroyMutexes();
-	pthread_exit(NULL);
+	destroy_mutexes();
 }
