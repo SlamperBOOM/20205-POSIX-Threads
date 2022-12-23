4c3916f5d632c976180720a00ed55a99b5af7522
mentalMint <walkershush@gmail.com>
2022-12-23 13:31:47 +0700

    chore: remove tests from directory

--- shuvalov/31/tests/tests/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clang++ -Wall -pedantic -std=c++2a tests.cpp proxy_tests.cpp -lpthread -lgtest -lcurl -o tests
-echo "Program tests compiled"
-
--- shuvalov/31/tests/tests/tests.cpp
+++
@@ -1,9 +0,0 @@
-#include "proxy_tests.h"
-
-/*
- * usage: ./tests <proxy_port>
- */
-int main(int argc, char *argv[]) {
-    RunAllTests(argc, argv);
-    return 0;
-}
