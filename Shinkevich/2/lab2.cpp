c3d00f583a9c183837599cbbd819f0d84332df32
SlamperBOOM <slavashink@gmail.com>
2022-12-26 22:06:21 +0700

    Add files via upload

---
+++ Shinkevich/2/lab2.cpp
@@ -0,0 +1,42 @@
+//Модифицируйте программу упр. 1 так, чтобы вывод 
+//родительской нити производился после завершения дочерней. 
+//Используйте pthread_join.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void* thr_fn(void* arg)
+{
+	int i;
+	for (i = 0; i < 10; ++i)
+	{
+		printf("Child thread\n");
+	}
+	pthread_exit((void*)0);
+}
+
+int main()
+{
+	int err, i;
+	void* tret;
+	pthread_t ntid;
+	err = pthread_create(&ntid, NULL, thr_fn, NULL);
+	if (err != 0)
+	{
+		printf("невозможно создать поток");
+		exit(1);
+	}
+	err = pthread_join(ntid, &tret);
+	if (err != 0)
+	{
+		printf("unable to join thread");
+		exit(1);
+	}
+	for (i = 0; i < 10; ++i)
+	{
+		printf("Parent thread\n");
+	}
+	exit(0);
+}
